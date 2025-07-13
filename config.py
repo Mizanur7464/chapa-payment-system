0fdc2ba31350d3e0315c0df47fc6311ee277b31f
Mizanur7464 <mdmizanurrahman1860@gmail.com>
2025-07-14 00:23:10 +0600

    first commit

---
+++ config.py
@@ -0,0 +1,11 @@
+# এখানে সরাসরি টোকেন, ইউজারনেম, লিংক দিন
+BOT_TOKEN = "8043786816:AAE_qkeIyjRfRt0pN7AbsM9-4YEyqaufOmo"
+# নিচের আইডিগুলো আপনার চ্যানেল ও গ্রুপের আসল আইডি দিয়ে প্রতিস্থাপন করুন
+CHANNEL_USERNAME = "-1002659408866"  # চ্যানেলের আইডি (উদাহরণ)
+CHANNEL_LINK = "https://t.me/nanabets1"
+
+GROUP_USERNAME = "-1002582748190"    # গ্রুপের আইডি (উদাহরণ)
+GROUP_LINK = "https://t.me/Nanabetchat"
+
+BOT_NAME = "@demoforwork_bot"
+BOT_DESCRIPTION = "A bot that requires users to join a channel and a group before proceeding"
